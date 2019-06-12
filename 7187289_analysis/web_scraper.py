f2f03982767f8dcd99d5fb4ffdbb462592f89c9b
j-berg <j-berg@users.noreply.github.com>
2019-06-11 19:17:39 -0600

    Update

--- 7187289_analysis/web_scraper.py
+++ 7187289_analysis/web_scraper.py
@@ -2,10 +2,12 @@
 
 
 # Gene dictionary accessed from https://biomart.genenames.org
-
 import os
 import sys
+import ast
 import pandas as pd
+import numpy as np
+from urllib.request import Request, urlopen
 
 __path__  =  os.path.dirname(os.path.realpath(__file__)) + '/'
 __path__ = '/Users/jordan/Desktop/xpressyourself_manuscript/7187289_analysis/'
@@ -19,9 +21,7 @@ del df.index.name
 df = df[['NCBI gene ID', 'UniProt accession']]
 df['NCBI gene ID'] = df['NCBI gene ID'].astype(pd.Int32Dtype())
 
-df.head()
-
-# Set gene list 
+# Set gene list
 gene_list = [
     'AC097634.4',
     'ADAP1',
@@ -50,55 +50,224 @@ gene_list = [
     'WNK4']
 
 
-gene_id = '29974'
-url = "https://www.ncbi.nlm.nih.gov/gene?cmd=Retrieve&dopt=full_report&list_uids=" + str(gene_id)
+df['index'] = df.index
+df_keep = df.drop_duplicates(subset='index', keep='first')
+df_genes = df_keep.reindex(gene_list)
+df_genes = df_genes.drop('index', axis=1)
 
-from urllib.request import Request, urlopen
+hot_terms_1 = [
+    'brain',
+    'neuron',
+    'central nervous system',
+    'synaptic',
+    'sympathetic',
+    'adrenergic',
+    'cerebellum',
+    'cerebral',
+    'dendritic',
+    'astrocyte',
+    'excitatory',
+    'substantia nigra',
+    'red nucleus',
+    'hippocampus',
+    'spinal cord',
+    'motor neuron',
+    'cerebral cortex',
+    'forebrain',
+    'nerve',
 
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    'neuro',
+    'neurological',
+    'postsynaptic',
+    'synaptic',
+    'neurotransmitter',
+    'neurotoxic',
+    'neuron migration',
+    'neuron projection',
+    'neurodegenerative',
+    'psychomotor',
+    'psych',
+    'noradrenergic',
+    'sympathetic',
+    'synaptic',
+    ''
 
-webpage = urlopen(req).read()
-
-
-
-
-
-
-
-go to 'https://www.genecards.org/cgi-bin/carddisp.pl?gene=' + str(gene_name)
-
-    go to 'href="http://www.ncbi.nlm.nih.gov/' URL
-    go to 'href="http://www.uniprot.org/' URL
-
-get UNIPROT
-    read in gene dictionary
-    search genes and scrape html
-    'http://www.uniprot.org/uniprot/' + str(uni_id) + '#function'
-
-
-
-
-
-
-
-
+    'epilepsy',
+    'alzheimer',
+    'encephalopathy',
+    'schizophrenia',
+    'encephalomyopathic',
+    'parkinson',
+    'retardation',
+    'autistic',
+    'autism',
 
 
-    parse sentance by sentence
-        look for items in hit_list
-            if 'association' or 'associated' in sentance
-                (1)
-            else:
-                (2)
+]
+
+hot_terms_2 = [
+    'calcium',
+    'ca2+',
+    'ca(2+)',
+    'sodium',
+    'nacl',
+    'potassium',
+    'k+',
+    'k(+)',
+    'channels',
+
+    'chemotactic',
+    'receptor',
+    'signaling',
+    'signal transduction',
+
+    'glutamate',
+
+    'behavior'
+
+
+]
+
+penalty = [
+    'associated',
+    'association',
+    'associate'
+
+
+]
 
 
 
-    if 'behavior/neurological phenotype' or 'nervous system phenotype' in file
+# Score gene based on NCBI gene summary
+for gene_name in df_genes.index.tolist():
 
-    go to 'mRNA expression in embryonic tissues'
-        hit_list = brain, neural, cerebellum, cerebral, neuron
-        (1) cehck for 'mRNA expression in embryonic tissues' for hit_list
+    gene_id = df_genes.at[gene_name, 'NCBI gene ID']
+    url = 'https://www.ncbi.nlm.nih.gov/gene/' + str(gene_id)
 
-        (2) check for 'This gene is overexpressed' for hit_list
+    try:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        page = webpage.decode('utf-8').splitlines()
 
-        (1) check for 'Gene Ontology (GO) annotations related' for hit_list
+        score_1 = None
+        score_2 = None
+        score_3 = None
+        for line in range(len(page) + 1):
+            if '</html>' in page[line]:
+                break
+
+            if '<dt>Summary</dt>' in page[line]:
+                score_1 = 0
+                score_2 = 0
+                score_3 = 0
+                for x in hot_terms_1:
+                    if x.lower() in page[line+1].lower():
+                        score_1 += 2
+
+                for y in hot_terms_2:
+                    if y.lower() in page[line+1].lower():
+                        score_2 += 1
+
+                for z in penalty:
+                    if z.lower() in page[line+1].lower():
+                        score_3 -= 0.5
+
+                # If only hit the penalty, revert back to zero
+                if score < 0:
+                    score = 0
+                df_genes.at[gene_name, 'NCBI_SCORE_1'] = score_1
+                df_genes.at[gene_name, 'NCBI_SCORE_2'] = score_2
+                df_genes.at[gene_name, 'NCBI_SCORE_PENALTY'] = score_3
+                break
+
+            if 'var tissues_data =' in page[line]:
+                exp_data = ast.literal_eval(page[line][27:-1])
+                exp_df = pd.DataFrame.from_dict(exp_data,orient='index')
+                exp_len = len(exp_df.index.tolist())
+                exp_loc = pd.DataFrame.from_dict(exp_data,orient='index').sort_values('full_rpkm', ascending=False).index.get_loc('brain')
+                exp_rank = (exp_len - exp_loc) / exp_len
+                df_genes.at[gene_name, 'NCBI_EXPRESSION_RANK'] = exp_rank
+
+    except:
+        df_genes.at[gene_name, 'NCBI_SCORE_1'] = None
+        df_genes.at[gene_name, 'NCBI_SCORE_2'] = None
+        df_genes.at[gene_name, 'NCBI_SCORE_PENALTY'] = None
+        df_genes.at[gene_name, 'NCBI_EXPRESSION_RANK'] = None
+
+
+df_genes
+
+
+
+
+
+
+
+
+
+
+
+# Score gene based on UNIPROT gene summary
+for gene_name in df_genes.index.tolist():
+
+    uniprot_id = df_genes.at[gene_name, 'UniProt accession']
+    url = 'https://www.uniprot.org/uniprot/' + str(uniprot_id)
+
+    try:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        webpage = urlopen(req).read()
+        page = webpage.decode('utf-8').splitlines()
+
+        score_1 = None
+        score_2 = None
+        score_3 = None
+        for line in range(len(page) + 1):
+            if '</html>' in page[line]:
+                break
+
+            if 'This section provides any useful information about the protein, mostly biological knowledge' in page[line] or 'This subsection of the ‘Pathology and Biotech’ section provides information' in page[line]:
+                score_1 = 0
+                score_2 = 0
+                score_3 = 0
+                for x in hot_terms_1:
+                    if x.lower() in page[line].lower():
+                        score_1 += 2
+
+                for y in hot_terms_2:
+                    if y.lower() in page[line].lower():
+                        score_2 += 1
+
+                for z in penalty:
+                    if z.lower() in page[line].lower():
+                        score_3 -= 0.5
+
+                # If only hit the penalty, revert back to zero
+                if score_3 < 0:
+                    score_3 = 0
+                df_genes.at[gene_name, 'UNIPROT_SCORE1'] = score_1
+                df_genes.at[gene_name, 'UNIPROT_SCORE2'] = score_2
+                df_genes.at[gene_name, 'UNIPROT_SCORE_PENALTY'] = score_3
+                break
+
+            exp_score = 0
+            if 'This subsection of the ‘Expression’ section provides information on the expression of the gene product' in page[line]:
+                for x in hot_terms_1:
+                    if x.lower() in page[line].lower():
+                        exp_score += 1
+
+            df_genes.at[gene_name, 'UNIPROT_EXPRESSION'] = exp_score
+
+
+    except:
+        df_genes.at[gene_name, 'UNIPROT_SCORE1'] = None
+        df_genes.at[gene_name, 'UNIPROT_SCORE2'] = None
+        df_genes.at[gene_name, 'UNIPROT_SCORE_PENALTY'] = None
+        df_genes.at[gene_name, 'UNIPROT_EXPRESSION'] = None
+
+
+
+
+
+
+
+df_genes
